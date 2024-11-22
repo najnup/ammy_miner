104fe0e75b895b0fbbc8c4fbfcc2fb8ae31f5024
najnup <89747703+najnup@users.noreply.github.com>
2024-11-22 22:30:04 +0200

    Update agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -1,1 +1,16 @@
-import request
+import requests
+import time
+import json
+from ppadb.client import Client as AdbClient
+
+"""
+TODO Start and stop miner application on the phones
+TODO Have Battery checks for all connected phones
+"""
+
+# Default is "127.0.0.1" and 5037
+client = AdbClient(host="127.0.0.1", port=5037)
+
+# Listing all connected devices
+devices = client.devices()
+
